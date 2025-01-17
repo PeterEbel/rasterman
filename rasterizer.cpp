cdb55fd4d94eb853743d274257907546809cb668
Peter Ebel <peter.ebel@outlook.de>
2025-01-17 14:37:02 +0100

    documentation

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,3 +1,16 @@
+// Rasterman Standalone Version 1.0
+// Copyright (C) 2025 Peter Ebel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 #include "rasterizer.h"
 #include <QDebug>
 #include <QFile>
